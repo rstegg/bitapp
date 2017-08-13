daa2e33869ff61636d6ed63aeafd6e54c4cfe7a3
Galileo Sanchez <galileopy@gmail.com>
2017-08-12 17:58:30 -0700

    seeders work

--- seeders/20170813000614-test-user.js
+++ seeders/20170813000614-test-user.js
@@ -1,14 +1,31 @@
 'use strict';
-
+const now = () => (new Date()).toISOString()
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('User', [{
-      name: 'John Doe',
-      isBetaMember: false
+    return queryInterface.bulkInsert('Users', [{
+      id: 1,
+      name: 'Galileo Sanchez',
+      phone: "+1213531958",
+      password: "$2a$10$2EL7Vm9lsi4gjpIAsMnPCeEwq6jgStTDgRb/CoU3YdvpjnitPCtxC",
+      registered: true,
+      verified: true,
+      verifyCode: "1234",
+      createdAt: now(),
+      updatedAt: now(),
+    }, {
+      id: 2,
+      name: 'Ryan Stegmann',
+      phone: "+19519922715",
+      password: "$2a$10$egNaDMBMrRwiK42U47kGeuixGL27wdg2OHt9Z9uqoHr26wgSSgx2m",
+      registered: true,
+      verified: true,
+      verifyCode: "1234",
+      createdAt: now(),
+      updatedAt: now(),
     }], {});
   },
 
   down: function (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Person', null, {});
+    return queryInterface.bulkDelete('Users', null, {});
   }
 };
